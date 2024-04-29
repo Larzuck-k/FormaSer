90db6b1dbb08a3a6051d62059a2452e52d7489cb
Larz <larzuck41@gmail.com>
2024-04-29 06:15:09 -0500

    nueva consulta

--- controlador/leerexcel.php
+++ controlador/leerexcel.php
@@ -170,7 +170,7 @@ for ($i=0; $i < count($elementos); $i++) {
               foreach ($array as $index => $value) {
           $cursados .=    $array[0]["nombre_curso"] . " ". $array[0]["ficha"] . " " .$array[0]["inscripcion"] . "  " . $array[0]["documento"] ;
                
-//SELECT * FROM cursos_aprendiz  INNER JOIN ingresados ON cursos_aprendiz.documento = ingresados.documento   INNER JOIN fichas ON cursos_aprendiz.ficha = fichas.ficha    where cursos_aprendiz.documento = 1112765623  and inscripcion between '2024-01-01' and '2024-12-31'
+//SELECT fichas.ficha as "Ficha anterior",fichas.nombre_curso, ingresados.ficha as "Ficha a inscribir" FROM cursos_aprendiz  INNER JOIN ingresados ON cursos_aprendiz.documento = ingresados.documento   INNER JOIN fichas ON cursos_aprendiz.ficha = fichas.ficha    where cursos_aprendiz.documento = 1112765623  and inscripcion between '2024-01-01' and '2024-12-31'
 //2 fichas apartes
               }
                   
